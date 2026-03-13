18590a0597b9bf7fab000959e1195750982f52e5
AhiruNeko <AhiruNeko@outlook.com>
2026-03-13 23:47:02 +0800

    add redirect to

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 // src/lib/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://xyzovwbnldmjjrjbowxx.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5em92d2JubGRtampyamJvd3h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3OTEyOTIsImV4cCI6MjA4ODM2NzI5Mn0.5xcRkBlrJwJP0jQN1KGt3yz_5adCVCfoGTtEvr7H2qw";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
