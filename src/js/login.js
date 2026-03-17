5518b81aba0ed579ca606f7404e27dccf37d4b67
AhiruNeko <AhiruNeko@outlook.com>
2026-03-17 18:45:53 +0800

    update login

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,9 @@
-import { signInWithGoogle, supabase } from './supabase.js';
+import { signInWithGoogle, getCurrentUser } from './supabase.js';
+
+const user = await getCurrentUser();
+if (user) {
+    window.location.href = 'account.html';
+}
 
 const loginBtn = document.getElementById('loginBtn');
 loginBtn.addEventListener('click', async () => {
